9865ddafe9d3147661b8bfa2da382b4386398852
Robin Stocker <robin@nibor.org>
2020-12-18 16:33:58 +1100

    Day 17: Some optimizations for fun

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,7 +1,7 @@
 //! https://adventofcode.com/2020/day/17
 
 use reformation::Reformation;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let input = include_str!("../../input/2020/day17.txt");
@@ -54,8 +54,6 @@ fn solve(puzzle: &Puzzle) -> usize {
                     let active = active_neighbors(pos, &map);
                     if active == 2 || active == 3 {
                         new_map.insert(pos.clone(), State::Active);
-                    } else {
-                        new_map.insert(pos.clone(), State::Inactive);
                     }
 
                     for neighbor in neighbor_positions(pos) {
@@ -67,8 +65,6 @@ fn solve(puzzle: &Puzzle) -> usize {
                 State::Inactive => {
                     if active_neighbors(pos, &map) == 3 {
                         new_map.insert(pos.clone(), State::Active);
-                    } else {
-                        new_map.insert(pos.clone(), State::Inactive);
                     }
                 }
             }
@@ -109,39 +105,43 @@ fn active_neighbors(pos: &(i64, i64, i64), map: &HashMap<(i64, i64, i64), State>
 }
 
 fn solve2(puzzle: &Puzzle) -> usize {
+    let diffs = calculate_diffs();
+
     let mut map = puzzle.map4.clone();
 
     for _cycle in 0..6 {
         let mut new_map = HashMap::new();
-        let mut also_check = Vec::new();
+        let mut inactive_neighbors = HashSet::new();
         for (pos, state) in &map {
             match state {
                 State::Active => {
-                    let active = active_neighbors4(pos, &map);
+                    let active = active_neighbors4(pos, &map, &diffs);
                     if active == 2 || active == 3 {
                         new_map.insert(pos.clone(), State::Active);
-                    } else {
-                        new_map.insert(pos.clone(), State::Inactive);
                     }
 
-                    for neighbor in neighbor_positions4(pos) {
-                        if !map.contains_key(&neighbor) {
-                            also_check.push(neighbor);
+                    for diff in &diffs {
+                        let p = (
+                            pos.0 + diff.0,
+                            pos.1 + diff.1,
+                            pos.2 + diff.2,
+                            pos.3 + diff.3,
+                        );
+                        if !map.contains_key(&p) {
+                            inactive_neighbors.insert(p);
                         }
                     }
                 }
                 State::Inactive => {
-                    if active_neighbors4(pos, &map) == 3 {
+                    if active_neighbors4(pos, &map, &diffs) == 3 {
                         new_map.insert(pos.clone(), State::Active);
-                    } else {
-                        new_map.insert(pos.clone(), State::Inactive);
                     }
                 }
             }
         }
 
-        for pos in also_check {
-            if active_neighbors4(&pos, &map) == 3 {
+        for pos in inactive_neighbors {
+            if active_neighbors4(&pos, &map, &diffs) == 3 {
                 new_map.insert(pos, State::Active);
             }
         }
@@ -152,8 +152,8 @@ fn solve2(puzzle: &Puzzle) -> usize {
     map.values().filter(|s| matches!(s, State::Active)).count()
 }
 
-fn neighbor_positions4((x, y, z, a): &(i64, i64, i64, i64)) -> Vec<(i64, i64, i64, i64)> {
-    let mut neighbors = Vec::new();
+fn calculate_diffs() -> Vec<(i64, i64, i64, i64)> {
+    let mut diffs = Vec::new();
     for xd in vec![-1, 0, 1] {
         for yd in vec![-1, 0, 1] {
             for zd in vec![-1, 0, 1] {
@@ -161,21 +161,23 @@ fn neighbor_positions4((x, y, z, a): &(i64, i64, i64, i64)) -> Vec<(i64, i64, i6
                     if xd == 0 && yd == 0 && zd == 0 && ad == 0 {
                         continue;
                     }
-                    neighbors.push((x + xd, y + yd, z + zd, a + ad));
+                    diffs.push((xd, yd, zd, ad));
                 }
             }
         }
     }
-    neighbors
+    diffs
 }
 
 fn active_neighbors4(
     pos: &(i64, i64, i64, i64),
     map: &HashMap<(i64, i64, i64, i64), State>,
+    diffs: &Vec<(i64, i64, i64, i64)>,
 ) -> usize {
-    neighbor_positions4(pos)
+    diffs
         .iter()
-        .filter(|p| map.get(p).map_or(false, |s| matches!(s, State::Active)))
+        .map(|d| (pos.0 + d.0, pos.1 + d.1, pos.2 + d.2, pos.3 + d.3))
+        .filter(|p| map.get(&p).map_or(false, |s| matches!(s, State::Active)))
         .count()
 }
 
